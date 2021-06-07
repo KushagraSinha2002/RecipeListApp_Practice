9694683c3c5e5afa5a0386e1bc419ab01ab18a5b
Kushagra Sinha <kushagrasinha2002@gmail.com>
2021-06-07 09:02:35 +0530

    Initial Commit

---
+++ Practice/Model/Recipe.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeModel.swift
+//  Practice
+//
+//  Created by kushagra sinha on 07/06/21.
+//
+
+import Foundation
---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by kushagra sinha on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
