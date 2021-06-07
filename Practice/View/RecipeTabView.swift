8620f4c3e83534ddec950e9c52ffb1629710ee88
Kushagra Sinha <kushagrasinha2002@gmail.com>
2021-06-07 12:29:40 +0530

    -Add RecipeTabView inside View

---
+++ Practice/View/RecipeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeTabView.swift
+//  Practice
+//
+//  Created by kushagra sinha on 07/06/21.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
