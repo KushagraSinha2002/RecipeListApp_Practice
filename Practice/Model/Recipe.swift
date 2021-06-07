1dbf2661056a414a9a92dbbaee4c55f3c6ae0849
Kushagra Sinha <kushagrasinha2002@gmail.com>
2021-06-07 09:16:01 +0530

    -Add Class Recipe under Recipe.swift -Add models under Class Recipe

--- Practice/Model/Recipe.swift
+++ Practice/Model/Recipe.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class Recipe: Identifiable, Decodable {
+    
+    var id: UUID?
+    var name: String
+    var featured: Bool
+    var image: String
+    var description: String
+    var prepTime: String
+    var cooktime: String
+    var totalTime: String
+    var servings: Int
+    var ingredients: [String]
+    var directions: [String]
+}
