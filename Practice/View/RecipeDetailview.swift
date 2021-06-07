541b2f0560349ed77c1d4452a1fdfc91ca8a7852
Kushagra Sinha <kushagrasinha2002@gmail.com>
2021-06-07 11:44:24 +0530

    -Add RecipeDetailView under Views Folder

---
+++ Practice/View/RecipeDetailview.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeDetailview.swift
+//  Practice
+//
+//  Created by kushagra sinha on 07/06/21.
+//
+
+import SwiftUI
+
+struct RecipeDetailview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeDetailview_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailview()
+    }
+}
